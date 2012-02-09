b1e237748cc94027c4e325a2183bf9a0d7f0d7b5
craigisrael <craig@theisraels.net>
2012-02-09 00:03:17 -0600

    added tests to verify that rental lines on statement are correct

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -19,13 +19,20 @@ describe Customer do
 
       it 'statement should end with footer' do
         lines = customer.statement.split("\n")
-        lines[-2].should =~  /Amount owed is 1.5/ 
-        lines[-1].should =~ /You earned \d+ frequent renter points/
+        lines[-2].should ==  "Amount owed is #{customer.rentals.map(&:total_cost).reduce(&:+)}" 
+        lines[-1].should == "You earned #{customer.rentals.map(&:renter_points_earned).reduce(&:+)} frequent renter points"
       end
 
-      it 'should include the rentals' do
+      it 'should include a single rental' do
         lines = customer.statement.split("\n")
-        lines[1].should =~ /\s*Shrek\s*\d+.*/
+        lines[1].should == "\tShrek\t#{customer.rentals[0].total_cost}"
+      end
+
+      it 'should include multiple rentals' do
+        customer.add_rental(Rental.new(regular_movie, 2))
+        lines = customer.statement.split("\n")
+        lines[1].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
+        lines[2].should == "\t#{customer.rentals[1].movie.title}\t#{customer.rentals[1].total_cost}"
       end
 
       it 'should calculate the total points for multiple rentals' do
