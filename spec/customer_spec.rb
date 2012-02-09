fb249b196e2d4e490efbcce4e0db8b66d48e97f1
craigisrael <craig@theisraels.net>
2012-02-08 22:27:48 -0600

    added tests for statement method

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,9 +1,30 @@
 require File.join(File.dirname(__FILE__), '../customer.rb')
 
 describe Customer do
-  it 'statement should not be empty' do
-    c = Customer.new('Craig')
-    c.add_rental(Rental.new(Movie.new('Shrek', Movie::CHILDRENS), 2))
-    c.statement.should_not be_empty
+
+  let(:customer) { Customer.new('Craig') }
+  let(:movie) { Movie.new('Shrek', Movie::CHILDRENS) }
+  let(:rental) { Rental.new(movie, 2) }
+
+  describe '#statement' do
+    before :each do
+      customer.add_rental(rental)
+    end
+
+    it 'statement should start with header' do
+      customer.statement.should =~ /Rental Record for Craig\n.*/
+    end
+
+    it 'statement should end with footer' do
+      lines = customer.statement.split("\n")
+      lines[-2].should =~  /Amount owed is 1.5/ 
+      lines[-1].should =~ /You earned \d+ frequent renter points/
+    end
+
+    it 'should include the rentals' do
+      lines = customer.statement.split("\n")
+      lines[1].should =~ /\s*Shrek\s*\d+.*/
+
+    end
   end
 end
