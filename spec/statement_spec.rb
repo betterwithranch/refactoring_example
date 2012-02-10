08ed30175ac35c0d3d055034e735b3b4f478cdf5
craigisrael <craig@theisraels.net>
2012-02-09 21:54:55 -0600

    changed tests to cover header, body and footer of statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,40 +1,57 @@
 require File.join(File.dirname(__FILE__), '../customer.rb')
 
 describe Statement do
-  describe '#generate' do
-    let(:customer) { Customer.new('Craig') }
-    let(:childrens_movie) { Movie.new('Shrek', Movie::CHILDRENS) }
-    let(:regular_movie) { Movie.new('Star Wars', Movie::REGULAR) }
-    let(:new_release_movie) { Movie.new('Chronicle', Movie::NEW_RELEASE) }
+  let(:customer) { Customer.new('Craig') }
+  let(:childrens_movie) { Movie.new('Shrek', Movie::CHILDRENS) }
+  let(:regular_movie) { Movie.new('Star Wars', Movie::REGULAR) }
+  let(:new_release_movie) { Movie.new('Chronicle', Movie::NEW_RELEASE) }
 
-    before :each do
-      customer.add_rental(Rental.new(childrens_movie, 2))
-    end
+  before :each do
+    customer.add_rental(Rental.new(childrens_movie, 2))
+  end
 
-    it 'statement should start with header' do
+  describe '#header' do
+    it 'should return the correct text' do
       statement = Statement.new(customer)
-      statement.generate.should =~ /Rental Record for Craig\n.*/
+      statement.generate
+      statement.header.should == "Rental Record for #{customer.name}\n"
     end
+  end
 
-    it 'statement should end with footer' do
+  describe '#footer' do
+    it 'should return the correct text' do
       statement = Statement.new(customer)
-      lines = statement.generate.split("\n")
-      lines[-2].should ==  "Amount owed is #{customer.rentals.map(&:total_cost).reduce(&:+)}" 
-      lines[-1].should == "You earned #{customer.rentals.map(&:renter_points_earned).reduce(&:+)} frequent renter points"
+      statement.generate
+      lines = statement.footer.split("\n")
+      lines[0].should ==  "Amount owed is #{customer.rentals.map(&:total_cost).reduce(&:+)}" 
+      lines[1].should == "You earned #{customer.rentals.map(&:renter_points_earned).reduce(&:+)} frequent renter points"
     end
+  end
 
+  describe '#body' do
     it 'should include a single rental' do
       statement = Statement.new(customer)
-      lines = statement.generate.split("\n")
-      lines[1].should == "\tShrek\t#{customer.rentals[0].total_cost}"
+      statement.generate
+      lines = statement.body.split("\n")
+      lines[0].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
     end
 
     it 'should include multiple rentals' do
       customer.add_rental(Rental.new(regular_movie, 2))
       statement = Statement.new(customer)
-      lines = statement.generate.split("\n")
-      lines[1].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
-      lines[2].should == "\t#{customer.rentals[1].movie.title}\t#{customer.rentals[1].total_cost}"
+      statement.generate
+      lines = statement.body.split("\n")
+      lines[0].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
+      lines[1].should == "\t#{customer.rentals[1].movie.title}\t#{customer.rentals[1].total_cost}"
+    end
+  end
+
+  describe '#generate' do
+
+    it 'statement should be header, body, footer' do
+      statement = Statement.new(customer)
+      result = statement.generate
+      result.should == statement.header + statement.body + statement.footer
     end
 
     it 'should calculate the total points for multiple rentals' do
