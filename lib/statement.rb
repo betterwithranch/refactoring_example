8ad24af91a739a1ce077de2598ec6eec3b8f1ecc
craigisrael <craig@theisraels.net>
2012-02-09 21:20:43 -0600

    make sure statement reflects customer changes

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,7 +1,6 @@
 class Statement
   
-  # TODO: test seams - remove after refactor
-  attr_accessor :total_statement_amount
+  attr_accessor :total_amount
   attr_accessor :total_renter_points
 
   def initialize(customer)
@@ -9,33 +8,36 @@ class Statement
   end
 
   def generate
-    @total_statement_amount, @total_renter_points = 0, 0
+    @header, @body, @footer = nil, nil, nil
+    @total_amount, @total_renter_points = 0, 0
     header + body + footer
   end
 
   def header
-    "Rental Record for #{@customer.name}\n"
+    @header ||= "Rental Record for #{@customer.name}\n"
   end
 
   def body
-    result = ''
+    return @body if @body
+    @body = ''
     @customer.rentals.each do |element|
 
       # determine amounts for each line
       this_amount = element.total_cost
-      @total_statement_amount += this_amount
+      @total_amount += this_amount
 
       # add frequent renter points
       @total_renter_points += element.renter_points_earned
 
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      @body += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
     end
-    result
+    @body
   end
 
   def footer
-    result = "Amount owed is #{@total_statement_amount.to_s}\n" +
+    @footer ||= "Amount owed is #{@total_amount.to_s}\n" +
       "You earned #{@total_renter_points.to_s} frequent renter points"
   end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -7,50 +7,63 @@ describe Statement do
     let(:regular_movie) { Movie.new('Star Wars', Movie::REGULAR) }
     let(:new_release_movie) { Movie.new('Chronicle', Movie::NEW_RELEASE) }
 
-    context 'Statement output' do
-      before :each do
-        customer.add_rental(Rental.new(childrens_movie, 2))
-      end
+    before :each do
+      customer.add_rental(Rental.new(childrens_movie, 2))
+    end
 
-      it 'statement should start with header' do
-        statement = Statement.new(customer)
-        statement.generate.should =~ /Rental Record for Craig\n.*/
-      end
+    it 'statement should start with header' do
+      statement = Statement.new(customer)
+      statement.generate.should =~ /Rental Record for Craig\n.*/
+    end
 
-      it 'statement should end with footer' do
-        statement = Statement.new(customer)
-        lines = statement.generate.split("\n")
-        lines[-2].should ==  "Amount owed is #{customer.rentals.map(&:total_cost).reduce(&:+)}" 
-        lines[-1].should == "You earned #{customer.rentals.map(&:renter_points_earned).reduce(&:+)} frequent renter points"
-      end
+    it 'statement should end with footer' do
+      statement = Statement.new(customer)
+      lines = statement.generate.split("\n")
+      lines[-2].should ==  "Amount owed is #{customer.rentals.map(&:total_cost).reduce(&:+)}" 
+      lines[-1].should == "You earned #{customer.rentals.map(&:renter_points_earned).reduce(&:+)} frequent renter points"
+    end
 
-      it 'should include a single rental' do
-        statement = Statement.new(customer)
-        lines = statement.generate.split("\n")
-        lines[1].should == "\tShrek\t#{customer.rentals[0].total_cost}"
-      end
+    it 'should include a single rental' do
+      statement = Statement.new(customer)
+      lines = statement.generate.split("\n")
+      lines[1].should == "\tShrek\t#{customer.rentals[0].total_cost}"
+    end
 
-      it 'should include multiple rentals' do
-        customer.add_rental(Rental.new(regular_movie, 2))
-        statement = Statement.new(customer)
-        lines = statement.generate.split("\n")
-        lines[1].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
-        lines[2].should == "\t#{customer.rentals[1].movie.title}\t#{customer.rentals[1].total_cost}"
-      end
+    it 'should include multiple rentals' do
+      customer.add_rental(Rental.new(regular_movie, 2))
+      statement = Statement.new(customer)
+      lines = statement.generate.split("\n")
+      lines[1].should == "\t#{customer.rentals[0].movie.title}\t#{customer.rentals[0].total_cost}"
+      lines[2].should == "\t#{customer.rentals[1].movie.title}\t#{customer.rentals[1].total_cost}"
+    end
 
-      it 'should calculate the total points for multiple rentals' do
-        customer.add_rental(Rental.new(regular_movie, 2))
-        statement = Statement.new(customer)
-        statement.generate
-        statement.total_statement_amount.should == customer.rentals.map(&:total_cost).reduce(&:+)
-      end
+    it 'should calculate the total points for multiple rentals' do
+      customer.add_rental(Rental.new(regular_movie, 2))
+      statement = Statement.new(customer)
+      statement.generate
+      statement.total_amount.should == customer.rentals.map(&:total_cost).reduce(&:+)
+    end
 
-      it 'should calculate the total renter points for multiple rentals' do
-        customer.add_rental(Rental.new(regular_movie, 2))
-        statement = Statement.new(customer)
-        statement.generate
-        statement.total_renter_points.should == customer.rentals.map(&:renter_points_earned).reduce(&:+)
-      end
+    it 'should calculate the total renter points for multiple rentals' do
+      customer.add_rental(Rental.new(regular_movie, 2))
+      statement = Statement.new(customer)
+      statement.generate
+      statement.total_renter_points.should == customer.rentals.map(&:renter_points_earned).reduce(&:+)
+    end
+
+    it 'should return the same results when run twice' do
+      statement = Statement.new(customer)
+      first = statement.generate
+      second = statement.generate
+      first.should == second
+    end
+
+    it 'should recalculate if customer is changed' do
+      statement = Statement.new(customer)
+      first = statement.generate
+      customer.add_rental(Rental.new(regular_movie, 2))
+      second = statement.generate
+      first.should_not == second
     end
   end
 end
