5a1e55beaaf2fac80fcdf8303228e5649ad240b3
craigisrael <craig@theisraels.net>
2012-02-09 21:12:14 -0600

    refactored statement parts to separate methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,6 @@
 class Customer
   attr_reader :name
   
-  # TODO: test seams - remove after refactor
-  attr_accessor :total_statement_amount
-  attr_accessor :total_renter_points
-
   def initialize(name)
     @name = name
     @rentals = []
--- lib/statement.rb
+++ lib/statement.rb
@@ -9,27 +9,33 @@ class Statement
   end
 
   def generate
-    total_amount, frequent_renter_points = 0, 0
-    result = "Rental Record for #{@customer.name}\n"
+    @total_statement_amount, @total_renter_points = 0, 0
+    header + body + footer
+  end
+
+  def header
+    "Rental Record for #{@customer.name}\n"
+  end
+
+  def body
+    result = ''
     @customer.rentals.each do |element|
 
       # determine amounts for each line
       this_amount = element.total_cost
+      @total_statement_amount += this_amount
 
       # add frequent renter points
-      frequent_renter_points += element.renter_points_earned
+      @total_renter_points += element.renter_points_earned
 
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
     end
-
-    @total_statement_amount = total_amount
-    @total_renter_points = frequent_renter_points
-
-    # add footer lines
-    result += "Amount owed is #{total_amount.to_s}\n"
-    result += "You earned #{frequent_renter_points.to_s} frequent renter points"
     result
+  end
+
+  def footer
+    result = "Amount owed is #{@total_statement_amount.to_s}\n" +
+      "You earned #{@total_renter_points.to_s} frequent renter points"
   end
 end
