213043f0f380ee227903ba52f99defcad8181ef6
craigisrael <craig@theisraels.net>
2012-02-09 20:55:01 -0600

    moved statement logic to class

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,27 +19,10 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
-    result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
-
-      # determine amounts for each line
-      this_amount = element.total_cost
-
-      # add frequent renter points
-      frequent_renter_points += element.renter_points_earned
-
-      # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
-    end
-
-    @total_statement_amount = total_amount
-    @total_renter_points = frequent_renter_points
-
-    # add footer lines
-    result += "Amount owed is #{total_amount.to_s}\n"
-    result += "You earned #{frequent_renter_points.to_s} frequent renter points"
-    result
+    s = Statement.new(self)
+    report = s.generate
+    @total_statement_amount = s.total_statement_amount
+    @total_renter_points = s.total_renter_points
+    report
   end
 end
---
+++ spec/statement_spec.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__), '../customer.rb')
+
+describe Statement do
+end
